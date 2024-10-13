cff5069fb0dad1c9eae855b6e0c74dc6a26fcde0
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-13 15:17:10 -0600

    add: controller creation users

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+const {registerUser}  = require('../services/auth.service');
+
+const registerUserController = async (req, res) => {
+    try {
+        const user = req.body;
+        const newUser = await registerUser(user);
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+module.exports = {
+    registerUserController
+}
