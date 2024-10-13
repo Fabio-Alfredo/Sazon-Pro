2e321a9bbe1783100398366bbae451ffd1bdaedd
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-13 15:16:32 -0600

    add: usage route register user

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const dbConnection = require('./config/dbConnection');
+const authRouter = require('./routes/auth.route');
 
 const app = express();
 dbConnection();
+app.use(express.json());
+app.use(authRouter);
 
 const PORT = process.env.PORT || 3001;
 
