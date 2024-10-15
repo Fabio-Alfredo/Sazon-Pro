b5b623d558e288d10a9acce2e9546d91100e1859
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-15 09:48:21 -0600

    delete: function existe user deleted

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,10 +3,7 @@ const {hashPassword} = require('../utils/password.util');
 const {existsUser} = require('./user.service');
 
 const registerUser = async (user)=>{
-    const exist = await existsUser(user.email);
-    if(exist){
-        throw new Error('User already exists');
-    }
+
     const hashedPassword = await hashPassword(user.password);
     newUser = await UserRepository.create({...user, password: hashedPassword});
     return newUser;
