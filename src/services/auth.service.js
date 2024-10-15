d3eceeb7b1ba5ab36e9cae0cddbbc5ca41b8ca3a
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-14 22:46:17 -0600

    update: implemetation validation user unique

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,12 @@
 const UserRepository = require('../repositories/user.repository');
 const {hashPassword} = require('../utils/password.util');
+const {existsUser} = require('./user.service');
 
 const registerUser = async (user)=>{
+    const exist = await existsUser(user.email);
+    if(exist){
+        throw new Error('User already exists');
+    }
     const hashedPassword = await hashPassword(user.password);
     newUser = await UserRepository.create({...user, password: hashedPassword});
     return newUser;
