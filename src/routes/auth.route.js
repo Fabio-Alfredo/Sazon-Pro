669d64666f5b5a14a91f6745ccd45be7502c2afa
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-15 11:00:51 -0600

    add: validation middleware exist user

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,7 @@
 const route = require('express').Router();
 const { registerUserController } = require('../controllers/auth.controller');
+const { checkUser }= require('../middleware/checkUser.middleware')
 
-route.post('/register', registerUserController);
+route.post('/register', checkUser,  registerUserController);
 
 module.exports = route;
