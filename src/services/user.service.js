6eb8d155198614266c0462519a6576bd9631246b
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-15 09:49:58 -0600

    update: creation function exist user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,10 @@
 const UserRepository = require('../repositories/user.repository');
 
-const existsUser = async(email)=>{
+const existUser = async(email)=>{
     const exist = await UserRepository.exisUser(email);
     return exist;
 }
 
 module.exports = {
-    existsUser
+    existUser
 }
