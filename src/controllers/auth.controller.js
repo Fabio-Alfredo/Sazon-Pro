3aa41f51293d8a3cfb180b7c2822c3c0259e519c
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-15 09:47:39 -0600

    update: validation user in controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,13 @@
 const {registerUser}  = require('../services/auth.service');
+const {existUser} = require('../services/user.service');
 
 const registerUserController = async (req, res) => {
     try {
         const user = req.body;
+
+        const exist = await existUser(user.email);
+        if(exist) return res.status(400).json({message: 'User already exists'});
+        
         const newUser = await registerUser(user);
         res.status(201).json(newUser);
     } catch (err) {
