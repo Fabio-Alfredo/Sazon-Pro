7309dd19828e0df87137a2f220cb0ed0dffdd2bd
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-16 16:11:57 -0600

    delete: delete validation error in user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,6 @@ const UserRepository = require('../repositories/user.repository');
 
 const existUser = async(email)=>{
     const exist = await UserRepository.exisUser(email);
-    if(!exist) throw new Error('User does not exist');
     return exist;
 }
 
