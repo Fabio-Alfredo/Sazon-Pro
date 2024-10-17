015220d7b54eb753f695c8537a684d55fb6b571c
Fabio-Alfredo <00210121@uca.edu.sv>
2024-10-16 19:36:20 -0600

    delete: deleted route import middleware not exists

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,6 @@
 const route = require('express').Router();
 const { registerUserController,loginUserController  } = require('../controllers/auth.controller');
-const { checkUser }= require('../middleware/checkUser.middleware')
 
-route.post('/register', checkUser,  registerUserController);
+route.post('/register',  registerUserController);
 route.post('/login',  loginUserController);
 module.exports = route;
